ec38daf807ef70b099526103c626924c16bd0bc2
gaston santos <santos_gaston@aol.com>
2022-05-06 22:56:01 -0500

    cambi0

--- app.js
+++ app.js
@@ -1,92 +1,79 @@
-//Paso 1: Definimos variables de entrada y uso interno del algoritmo
-var nombreCompleto;
-var añoNacimiento, añoActual;
-var genero;
-var edad;
-var viveNortePais;
-var embarazada;
-var tieneNueveSemanasGestacion;
 
-//Paso 2: Solicitar datos al usuario
-nombreCompleto = prompt('Ingresa tu nombre completo');
-añoNacimiento = Number(prompt('Ingresa tu año de nacimiento'));
-añoActual = Number(prompt('Ingresa el año actual'));
-genero = prompt('Selecciona tu género: F=Femenino, M=Masculino');
-viveNortePais = prompt('¿Vives en un estado al norte al país? Respuesta: S o N');
+//  var arrays = [];// esta vacio
+// var valores = ['hola','mascota','comida','adios'];
 
-//Paso 3: Desarrollar la logica de negocio
-if(nombreCompleto !== '' && !isNaN(añoNacimiento) && !isNaN(añoActual) && genero !== '' && viveNortePais !== '')
-{
-    edad = añoActual - añoNacimiento;
+// for(var i = 0; i < arrays.length; i++){
+    // alert(arrays[i]);
+// }
 
-    //validamos si la persona es menor a 18 años
-    if(edad < 18)
-    {
-        alert(`Lo sentimos ${nombreCompleto}, aún no puedes vacunarte, porque eres menor de edad.`);
-    }
-    else
-    {
-        //validamos si vive en el norte del pais
-        if(viveNortePais.toUpperCase() === 'S')
-        {
-            //validamos si es Masculino o Femenino
-            if(genero.toUpperCase() === 'F')
-            {
-                embarazada = prompt('¿Estas embarazada? Respuesta: S o N');
-                
-                //validamos si esta embarazada o no
-                if(embarazada.toUpperCase() === 'S')
-                {
-                    tieneNueveSemanasGestacion = prompt('¿Tienes mas de 9 semanas de gestación? Respuesta: S o N');
+// var = Tacos , Alitas, Boneles, pozole, Chilaquiles, Tamales,
 
-                    //validamos si tiene mas de 9 semanas de gestacion
-                    if(tieneNueveSemanasGestacion.toUpperCase() === 'S')
-                    {
-                        if(edad >= 30)
-                        {
-                            alert(`Felicitaciones ${nombreCompleto}, ya puedes vacunarte`);
-                        }
-                        else
-                        {
-                            alert(`Lo sentimos ${nombreCompleto}, aún no puedes vacunarte, porque estamos en fase de mayores de 30 años.`);
-                        }
-                    }
-                    else
-                    {
-                        alert(`Lo sentimos ${nombreCompleto}, aún no puedes vacunarte, porque existe un riesgo para tu bebe.`);
-                    }
-                }
-                else
-                {
-                    if(edad >= 30)
-                    {
-                        alert(`Felicitaciones ${nombreCompleto}, ya puedes vacunarte`);
-                    }
-                    else
-                    {
-                        alert(`Lo sentimos ${nombreCompleto}, aún no puedes vacunarte, porque estamos en fase de mayores de 30 años.`);
-                    }
-                }
-            }
-            else
-            {
-                if(edad >= 30)
-                {
-                    alert(`Felicitaciones ${nombreCompleto}, ya puedes vacunarte`);
-                }
-                else
-                {
-                    alert(`Lo sentimos ${nombreCompleto}, aún no puedes vacunarte, porque estamos en fase de mayores de 30 años.`);
-                }
-            }
-        }
-        else
-        {
-            alert(`Lo sentimos ${nombreCompleto}, aún no puedes vacunarte porque no te encuentras en una zona habilitada para esto aún.`);
-        }    
-    }
-}
-else
-{
-    alert('Ups, te faltaron datos por ingresar, intentalo de nuevo');
-}
+//
+//
+////var valores = ['Tacos','Alitas','Boneless','Pozole','Chilaquiles','Tamales'];
+//
+//x = 1
+//switch (19){
+//    case 1:
+//        document.write('Sunday');
+//        break
+//        case 2:
+//        document.write('Monday');
+//        break
+//        case 3:
+//        document.write('Tusday');
+//        break
+//        case 4:
+//            document.write('Wednesday')
+//            break
+//            case 4:
+//            document.write('Thusday')
+//            break
+//            case 4:
+//            document.write('Fryday')
+//            break
+//            case 4:
+//            document.write('Saturday')
+//            break
+//            case 4:
+//            document.write('Sunday')
+//            break
+//            default :
+//            // document.write('The Document Is Not In The List')
+//
+//
+//}
+
+// operadores logicos
+//for (;;){
+      // if (x <=100){
+   // document.write(x, '<br />');
+   // x++;
+      // }else{
+          // break;
+      // }
+//}
+
+// whike loop in java
+//x = 1 ;
+//while (x<=35){
+//    
+// document.write(x, '<br />');
+// x++; //esto incrementa
+//}
+
+// Pop-up boxes in hava
+
+//x = prompt ("Please Enter Your Name : ");
+// if((x==null) || (x==' '));
+//    document.write("Sorry! You Have not entered any name. ")
+//else
+//    document.write('Hello '+x);
+
+// Document... while loop in hava
+
+
+
+
+
+
